fe5b474e5e70a7f62ff8d33a71716333f48c3ed3
ShaiqRaza <shaiqraza808y@gmail.com>
2024-11-17 12:00:46 +0500

    style update

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,9 @@ function App() {
           <div className='md:w-[450px] sm:w-[400px] w-4/5 flex flex-col gap-3 justify-center items-center'>
             <InputBox label="From" currencyKeys={currencyKeys} amount={amount} changeSelectedCurrency={setFrom} selectedCurrency={from} changeAmount={setAmount} />
             <InputBox label="To" currencyKeys={currencyKeys} amount={convertedAmount} changeSelectedCurrency={setTo} selectedCurrency={to} changeAmount={setAmount} setAmountDisability={true}/>
-            <button className='absolute bg-blue-700 text-white font-semibold px-2 py-1 rounded-md sm:text-base text-sm' type="button" onClick={swap}>SWAP</button>
+            <button className='absolute bg-blue-700 text-white font-semibold px-2 py-1 rounded-md sm:text-base text-sm hover:scale-105 hover:bg-blue-800' type="button" onClick={swap}>SWAP</button>
           </div>
-          <div className='bg-blue-700 md:w-[450px] sm:w-[400px] w-4/5 text-center h-[35px] rounded-md text-white mt-4' >Converting  {from} to  {to}</div>
+          <div className='bg-blue-700 md:w-[450px] sm:w-[400px] w-4/5 text-center h-[35px] rounded-md text-white mt-4 flex justify-center items-center' >Converting  {from} to  {to}</div>
         </div>
       </div>
     </>
